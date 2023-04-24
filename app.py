5fd5002c336c711f3918df5f285a4b725b26b0bb
Jacob Gulyaev <doom-e@bk.ru>
2023-04-24 19:05:49 +0700

    Add index template, rework authentication

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 import logging
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 import uvicorn
 
 from src.auth.routers import auth_router
+
+
+dirs_for_templates = Jinja2Templates(directory="static")
 
 
 host = "127.0.0.1"
@@ -14,12 +20,18 @@ logging.basicConfig(filename='loggs.log', level=logging.DEBUG, \
                         format="%(name)s - %(levelname)s:%(message)s")
 
 
+app = FastAPI()
+app.mount("/static",
+          StaticFiles(directory="static"),
+          name="static")
+app.include_router(auth_router)
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return dirs_for_templates.TemplateResponse("index.html",
+                                               {"request": request})
+
+
 if __name__ == '__main__':
-    app = FastAPI()
-    app.include_router(auth_router)
     uvicorn.run(app, host=host, port=port)
-
-
-@app.get("/")
-def get_menu():
-    return {"status": "success"}
--- src/auth/routers.py
+++ src/auth/routers.py
@@ -1,12 +1,17 @@
 import logging
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 from .auth import registration_user, authentication_user
 from .schemas import User, AuthUser, ResponseForAuth, ResponseForReg
 
 
 logger = logging.getLogger(name=__name__)
+
+
+templates = Jinja2Templates(directory="src/auth/templates")
 
 
 auth_router = APIRouter(
@@ -30,8 +35,13 @@ async def registration(user: User):
     return {"status": "success", "addable_user": user_json}
 
 
-@auth_router.post("/authentication", response_model=ResponseForAuth)
-async def authentication(user: AuthUser):
+@auth_router.get("/authentication", response_class=HTMLResponse)
+async def authentication(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@auth_router.post("/authentication/postdata", response_model=ResponseForAuth)
+async def authentication_post():
     if not authentication_user(email=user.email, password=user.password):
         return {
             "status": "not success",
